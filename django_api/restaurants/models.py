1f63395d94d3e14937295d5de55fc4d22e441aa2
Charles de la Roche <charlesr@theodo.fr>
2019-07-31 14:48:55 +0200

    :sparkles: create the restaurant model, generate migration and migrate

--- django_api/restaurants/models.py
+++ django_api/restaurants/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class Restaurant(models.Model):
+    name = models.CharField(max_length=200, null=False)
+
+    def __str__(self):
+        return self.name
